69e8c7ce213229f823820aa038c40e3bcd5f3867
Anmol-14 <anmolmogalai44@gmail.com>
2024-07-13 10:53:45 +0530

    Final Commit1

--- src/train_and_evaluate_mlflow.py
+++ src/train_and_evaluate_mlflow.py
@@ -3,6 +3,8 @@ import yaml
 import pandas as pd
 import numpy as np
 import argparse
+from pkgutil import get_data
+from get_data import get_data, read_params
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.linear_model import ElasticNet
@@ -10,13 +12,13 @@ import joblib
 import json
 import mlflow
 from urllib.parse import urlparse
-from get_data import get_data, read_params  # Assuming these are custom functions you've defined
+import mlflow
 
 def eval_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
     mae = mean_absolute_error(actual, pred)
     r2 = r2_score(actual, pred)
-    return rmse, mae, r2
+    return rmse,mae,r2
 
 def train_and_evaluate_mlops(config_path):
     config = read_params(config_path)
@@ -44,14 +46,15 @@ def train_and_evaluate_mlops(config_path):
 
     mlflow_config = config["mlflow_config"]
     remote_server_uri = mlflow_config["remote_server_uri"]
+    #remote_server_uri = "https://dagshub.com/TanzeemAgra/regression_model.mlflow"
     mlflow.set_tracking_uri(remote_server_uri)
 
     ######################### MLFLOW SERVER ########
     mlflow.set_experiment(mlflow_config["experiment_name"])
 
-    with mlflow.start_run(run_name=mlflow_config["run_name"]) as mlops_runs: 
+    with mlflow.start_run(run_name=mlflow_config["run_name"]) as mlosps_runs: 
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
-        lr.fit(train_x, train_y)
+        lr.fit(train_x,train_y)
 
         predicted_values = lr.predict(test_x)
 
@@ -64,20 +67,39 @@ def train_and_evaluate_mlops(config_path):
         mlflow.log_metric("mae", mae)
         mlflow.log_metric("r2", r2)
 
-        tracking_uri_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+        # score_files = config["reports"]["score"]
+        # params_file = config["reports"]["params"]
 
-        if tracking_uri_type_store != "file":
-            mlflow.sklearn.log_model(lr, "model", registered_model_name=mlflow_config["registered_model"])
+        # with open(score_files, "w") as f:
+        #     scores = {
+        #         "rmse" : rmse,
+        #         "mae" : mae,
+        #         "r2" : r2
+        #     }
+        #     json.dump(scores, f, indent=4)
+
+        # ######################
+
+        # with open(params_file, "w") as f:
+        #     scores = {
+        #         "alpha" : alpha,
+        #         "l1_ratio" : l1_ratio,
+        #     }
+        #     json.dump(scores, f, indent=4)
+
+        tracking_uri_type_store = urlparse(mlflow.get_artifact_uri()).scheme
+
+        if tracking_uri_type_store !="file" :
+            mlflow.sklearn.log_model(lr, "model", registered_model_name = mlflow_config["registered_model"])
         else:
             mlflow.sklearn.log_model(lr, "model")
 
     os.makedirs(model_dir, exist_ok=True)
-    model_path = os.path.join(model_dir, "model.joblib")
+    model_path = os.path.join(model_dir, "models.joblib")
     joblib.dump(lr, model_path)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", default="params.yaml", help="Path to the YAML configuration file")
-    args = parser.parse_args()
-
-    train_and_evaluate_mlops(args.config)
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args=args.parse_args()
+    train_and_evaluate_mlops(config_path=parsed_args.config)
